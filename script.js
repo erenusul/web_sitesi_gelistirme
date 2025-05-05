7cbcc98312cdc603097119cafbb70b4a686db8f4
Eren Usul <erenusul@gmail.com>
2025-05-06 00:49:10 +0300

    "Kolye, bilezik, tasarımlar linklerine tıklayınca o sayfalara yumuşak geçiş efektleri eklendi. "

--- script.js
+++ script.js
@@ -110,5 +110,20 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a.fade-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const href = this.getAttribute('href');
+            document.body.classList.add('fade-out');
+
+            setTimeout(() => {
+                window.location.href = href;
+            }, 500);
+        });
+    });
+});
 
 
